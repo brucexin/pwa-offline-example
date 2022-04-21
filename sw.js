109ae8462c04b7fde62ea7c52e4d336e2cab82b8
Bruce Xin <bruce.xin@gmail.com>
2022-04-21 15:39:47 +0800

    handle install/activate/fetch event, implement only cache mode

--- sw.js
+++ sw.js
@@ -1,23 +1,70 @@
-let VER = '0.0.1';
+let VER = '0.0.7';
+let CACHE_NAME = 'cache_'+VER; // An unique name in origin scope, 
+                               // see https://www.w3.org/TR/service-workers-1/#dfn-relevant-name-to-cache-map
 
-SITE_FILES = [
+let SITE_FILES = [
     '/icon.png',
-    'index.html',
-    'index.js'
-]
+    '/index.html',
+    '/index.js'
+];
 
+function log(...args) {
+    console.log(`[sw.js ${VER}]`, ...args);
+}
+
+async function doInstall() {
+    try {
+        let cache = await caches.open(CACHE_NAME);
+        // let oldkeys = await cache.keys();
+        // console.log('cache before install:', oldkeys);
+        let result = await cache.addAll(SITE_FILES);
+        let keys = await cache.keys();
+        console.log('cache after install:', keys);
+        return result;
+    } catch(err) {
+        return err;
+    }
+    
+}
 self.addEventListener('install', (e) => {
-    console.log('[sw.js] Install ', e);
+    console.log(`[sw.js ${VER}] Install `, e);
+    e.waitUntil(doInstall());
 });
 
+// delete old cache prevent storage leak
+async function doActivate() {
+    try {
+        let names = await caches.keys();
+        for(let name of names) {
+            if(name != CACHE_NAME) {
+                await caches.delete(name);
+                log('deleted old cache '+name);
+            }
+            
+        }
+        return;
+    } catch(err) {
+        return err;
+    }
+}
+
 self.addEventListener('activate', (e) => {
-    console.log('[sw.js] Activate ', e);
+    console.log(`[sw.js ${VER}] Activate `, e);
+    e.waitUntil(doActivate());
 });
 
 async function onFetch(evt) {
-    return await fetch(evt.request);
+    console.log("onFetch ", JSON.stringify(evt.request, null, '\t'));
+    let resp = await caches.match(evt.request);
+    if(resp) {
+        log('hit cache ', evt.request.url, resp);
+        return resp;
+    } else {
+        // default go to network
+        log('missed in cache', evt.request.url);
+    }
 }
 self.addEventListener('fetch', (e) => {
-    console.log('[sw.js] Fetch ', e);
+    console.log(`[sw.js ${VER}] Fetch `, e);
     e.respondWith(onFetch(e));
 });
