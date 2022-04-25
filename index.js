f0e010019d871ce5fb51001fc3355832dd504435
Bruce Xin <bruce.xin@gmail.com>
2022-04-25 21:14:56 +0800

    Let user conform reload immediate when service-worker new version installed.w

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-
+let version = "v1";
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('try register serviceWorker...');
+    document.getElementById('page-version').innerHTML = version;
     if('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js')
             .then((reg) => {
@@ -15,15 +16,31 @@ document.addEventListener('DOMContentLoaded', () => {
                     reg.active.onerror = (ev) => {
                         console.log("Service worker active error:", ev);
                     }
+                    reg.active.onstatechange = (ev) => {
+                        console.log("Service worker state changed:", ev);
+                    }
                 }
                 if(reg.installing) {
                     reg.installing.onerror = (ev) => {
                         console.log("Service worker installing error:", ev);
                     }
+                    reg.installing.onstatechange = (ev) => {
+                        console.log("Service worker state changed:", ev);
+                    }
                 }
                 if(reg.waiting) {
                     reg.waiting.onerror = (ev) => {
                         console.log("Service worker waiting error:", ev);
+                    }
+                    reg.waiting.onstatechange = (ev) => {
+                        console.log("Service worker state changed:", ev);
+                    }
+                }
+                reg.onupdatefound = (ev) => {
+                    console.log("Service worker has a new version:", ev);
+                    if(confirm("Has a new version, do u want update immediatly?")) {
+                        reg.installing.postMessage(
+                            JSON.stringify({"type":"SKIP_WAITING"}));
                     }
                 }
                 
@@ -38,7 +55,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById("text-version").innerHTML = cmd.value;
             }
         }
+        navigator.serviceWorker.onerror = (ev) => {
+            console.log("Service worker occured error:", ev);
+        }
+        let refreshing = false;
+        navigator.serviceWorker.oncontrollerchange = (ev) => {
+            console.log("Service worker changed:", ev);
+            if(refreshing) {
+                return;
+            }
+            refreshing = true;
+            window.location.reload();
+        }
+        navigator.serviceWorker.onmessageerror = (ev) => {
+            console.log("Service worker sent message error:", ev);
+        }
+        
+
     } else {
         console.error('Browser not support service worker!!!');
+    }
+
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    } else {
+        console.log("This browser support desktop notification")
     }
 });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let VER = '0.0.15';
+let VER = '0.0.28';
 let CACHE_NAME = 'cache_'+VER; // An unique name in origin scope, 
                                // see https://www.w3.org/TR/service-workers-1/#dfn-relevant-name-to-cache-map
 
@@ -62,6 +62,7 @@ async function onFetch(evt) {
     } else {
         // default go to network
         log('missed in cache', evt.request.url);
+        return;
     }
 }
 self.addEventListener('fetch', (e) => {
@@ -72,6 +73,9 @@ self.addEventListener('fetch', (e) => {
 async function handleMessage(client, cmd) {
     if(cmd.type == "VER") {
         client.postMessage(JSON.stringify({"type":"VER", "value":VER}));
+    } else if(cmd.type == "SKIP_WAITING") {
+        log("received skipWaiting command");
+        self.skipWaiting();
     }
 }
 
@@ -79,5 +83,68 @@ self.addEventListener("message", (e) => {
     let cmd = JSON.parse(e.data);
     log("received command from navigator:", e);
     e.waitUntil(handleMessage(e.source, cmd));
-    
 })
+
+self.addEventListener("messageerror", (e) => {
+    log("received unrecognize message ", e);
+})
+
+self.addEventListener("push", (e) => {
+    log("received push message ", e);
+})
+
+self.addEventListener("sync", (e) => {
+    log("received sync message ", e);
+})
+
+self.addEventListener("periodicsync", (e) => {
+    log("received push message ", e);
+})
+
+self.addEventListener("notificationclick", (e) => {
+    log("received notificationclick message ", e);
+})
+
+self.addEventListener("notificationclose", (e) => {
+    log("received notificationclose message ", e);
+})
+
+self.addEventListener("backgroundfetchabort", (e) => {
+    log("received backgroundfetchabort message ", e);
+})
+
+self.addEventListener("backgroundfetchclick", (e) => {
+    log("received backgroundfetchclick message ", e);
+})
+
+self.addEventListener("backgroundfetchfail", (e) => {
+    log("received backgroundfetchfail message ", e);
+})
+
+self.addEventListener("backgroundfetchsuccess", (e) => {
+    log("received backgroundfetchsuccess message ", e);
+})
+
+self.addEventListener("contentdelete", (e) => {
+    log("received contentdelete message ", e);
+})
+
+self.addEventListener("cookiechange", (e) => {
+    log("received cookiechange message ", e);
+})
+
+self.addEventListener("paymentrequest", (e) => {
+    log("received paymentrequest message ", e);
+})
+
+self.addEventListener("canmakepayment", (e) => {
+    log("received canmakepayment message ", e);
+})
+
+self.addEventListener("abortpayment", (e) => {
+    log("received canmakepayment message ", e);
+})
+
+
+
+
